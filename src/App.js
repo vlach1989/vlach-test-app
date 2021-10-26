9911ae160aa09882d9142311df5d91c5d2fc1722
PavelVlach <vlach1989@gmail.com>
2021-10-26 12:58:55 +0200

    epmpty app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import {Address, Name} from "vlach-test-package";
 
 const App = () => (
 	<>
-		<Name firstname="Martin" surname="Novák"/>
-		<Address street="Nová" city="Praha" country="Česko"/>
+		Ahoj
 	</>
 );
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 function renderApp() {
-	const rootEl = document.getElementById('root');
-	const render = ReactDOM.render;
-	render(<App/>, rootEl);
+	const rootNode = document.getElementById('root');
+	ReactDOM.render(<App />, rootNode);
 }
 
 renderApp();
